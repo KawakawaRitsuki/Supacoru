5fdf973dc774c824bbf991aa7547e54bf40bd004
RitsukiGoto <kawakawaritsuki.dev@gmail.com>
2015-06-02 00:36:16 +0900

    画面回転固定

--- app/src/main/java/com/kawakawaplanning/supacoru/Show.java
+++ app/src/main/java/com/kawakawaplanning/supacoru/Show.java
@@ -6,12 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
-import android.widget.Toast;
-
-import jp.sharakova.android.urlimageview.UrlImageView;
 
 /**
  * Created by KP on 15/05/31.
@@ -31,7 +26,7 @@ public class Show extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
 
-        vp = (ViewPager)findViewById(R.id.mypager);//定義
+        vp = (ViewPager) findViewById(R.id.mypager);//定義
         adap = new PAdapter(this.getSupportFragmentManager());
         vp.setAdapter(adap);//アダプタ入れる
 
@@ -39,124 +34,6 @@ public class Show extends ActionBarActivity {
         position = intent.getIntExtra("pos", 0);
         vp.setCurrentItem(position);
 
-//        context = this;
-
-//
-//        progressDialog = new ProgressDialog(this);
-//        progressDialog.setTitle("読み込み中...");
-//        progressDialog.setMessage("画像を読み込み中です。しばらくお待ち下さい。");
-//        progressDialog.setIndeterminate(false);
-//        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-//        progressDialog.setCancelable(false);
-//
-//
-//        Intent intent = getIntent();
-//        position = intent.getIntExtra("pos", 0);
-//
-//        items = MainActivity.adapter.getItem(position);
-//
-//        getSupportActionBar().setTitle(items.title);
-//
-//        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-//        NetworkInfo nInfo = cm.getActiveNetworkInfo();
-//
-//        if (nInfo != null) {
-//            /* NetWork接続可 */
-//            UrlImageView imageView = (UrlImageView) findViewById(R.id.showImg);
-//            imageView.setImageUrl(items.url, new UrlImageView.OnImageLoadListener() {
-//                @Override
-//                public void onStart(String url) {
-//                    progressDialog.show();
-//                }
-//
-//                @Override
-//                public void onComplete(String url) {
-//                    progressDialog.dismiss();
-//                }
-//
-//            });
-//
-//        } else {
-//            /* NetWork接続不可 */
-//            Toast.makeText(this, "No Network Connection!", Toast.LENGTH_LONG)
-//                    .show();
-//        }
-
-
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuItem backBtn = menu.add("back");
-        backBtn.setIcon(R.drawable.ic_menu_back);
-        backBtn.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-
-        MenuItem nextBtn = menu.add("next");
-        nextBtn.setIcon(R.drawable.ic_menu_next);
-        nextBtn.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-
-
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getTitle().toString()){
-            case "next":
-                if(position >= 1){
-                    position--;
-
-                    items = MainActivity.adapter.getItem(position);
-
-                    getSupportActionBar().setTitle(items.title);
-
-                    UrlImageView imageView = (UrlImageView) findViewById(R.id.showImg);
-                    imageView.setImageUrl(items.url, new UrlImageView.OnImageLoadListener() {
-                        @Override
-                        public void onStart(String url) {
-                            progressDialog.show();
-                        }
-
-                        @Override
-                        public void onComplete(String url) {
-                            progressDialog.dismiss();
-                        }
-                    });
-                }else{
-                    Toast.makeText(this,"最新話です",Toast.LENGTH_SHORT).show();
-                }
-
-                break;
-
-            case "back":
-                if(position < MainActivity.max-1){
-                    position++;
-
-                    items = MainActivity.adapter.getItem(position);
-
-                    getSupportActionBar().setTitle(items.title);
-
-                    UrlImageView imageView = (UrlImageView) findViewById(R.id.showImg);
-                    imageView.setImageUrl(items.url, new UrlImageView.OnImageLoadListener() {
-                        @Override
-                        public void onStart(String url) {
-                            progressDialog.show();
-                        }
-
-                        @Override
-                        public void onComplete(String url) {
-                            progressDialog.dismiss();
-                        }
-                    });
-                }else{
-                    Toast.makeText(this,"1話目です",Toast.LENGTH_SHORT).show();
-                }
-
-                break;
-        }
-        return true;
     }
 }
 
